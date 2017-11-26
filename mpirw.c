18385d0ad74527c31587860aa7989cf1389528bb
readyfreddie80 <33039341+readyfreddie80@users.noreply.github.com>
2017-11-26 17:41:07 +0300

    Update mpirw.c

--- mpirw.c
+++ mpirw.c
@@ -328,8 +328,8 @@ int main(int argc, char **argv) {
 
 
     int rank, size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     omp_set_num_threads(2);
 
     double start, end;
